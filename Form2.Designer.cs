bb383ba862f39b297440466ee662496c09c42666
ghiegie <giancarlogarcia.gallano@gmail.com>
2024-06-07 16:28:42 +0800

    06-07-2024

---
+++ Form2.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace AbzPos_C_
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            listView1 = new ListView();
+            user = new ColumnHeader();
+            pos = new ColumnHeader();
+            shift = new ColumnHeader();
+            off_on = new ColumnHeader();
+            panel3 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button3 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(9, 9);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(782, 47);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button7);
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(listView1);
+            panel2.Location = new Point(9, 56);
+            panel2.Margin = new Padding(0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(391, 385);
+            panel2.TabIndex = 1;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(346, 184);
+            button7.Name = "button7";
+            button7.Size = new Size(29, 23);
+            button7.TabIndex = 4;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(346, 155);
+            button6.Name = "button6";
+            button6.Size = new Size(29, 23);
+            button6.TabIndex = 3;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(346, 126);
+            button5.Name = "button5";
+            button5.Size = new Size(29, 23);
+            button5.TabIndex = 2;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(20, 339);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 1;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { user, pos, shift, off_on });
+            listView1.Location = new Point(0, 0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(391, 313);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // user
+            // 
+            user.Text = "User";
+            user.Width = 35;
+            // 
+            // pos
+            // 
+            pos.Text = "POS";
+            pos.Width = 34;
+            // 
+            // shift
+            // 
+            shift.Text = "Shift";
+            shift.Width = 36;
+            // 
+            // off_on
+            // 
+            off_on.Text = "offline";
+            off_on.Width = 282;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button12);
+            panel3.Controls.Add(button11);
+            panel3.Controls.Add(button10);
+            panel3.Controls.Add(button9);
+            panel3.Controls.Add(button8);
+            panel3.Controls.Add(tabControl1);
+            panel3.Location = new Point(400, 56);
+            panel3.Margin = new Padding(0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(391, 385);
+            panel3.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Controls.Add(button3, 2, 1);
+            tableLayoutPanel1.Controls.Add(button2, 1, 1);
+            tableLayoutPanel1.Controls.Add(label3, 2, 0);
+            tableLayoutPanel1.Controls.Add(label2, 1, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button1, 0, 1);
+            tableLayoutPanel1.Location = new Point(9, 9);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 31.9148941F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 68.0851059F));
+            tableLayoutPanel1.Size = new Size(782, 47);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(523, 18);
+            button3.Name = "button3";
+            button3.Size = new Size(254, 26);
+            button3.TabIndex = 5;
+            button3.Text = "SHIFT PAGE";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(263, 18);
+            button2.Name = "button2";
+            button2.Size = new Size(254, 26);
+            button2.TabIndex = 4;
+            button2.Text = "PAYMENT PAGE";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(523, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Shift";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(263, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Payment";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Invoice";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 18);
+            button1.Name = "button1";
+            button1.Size = new Size(254, 26);
+            button1.TabIndex = 3;
+            button1.Text = "INVOICE PAGE";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(388, 313);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Margin = new Padding(0);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(380, 285);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(380, 285);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(4, 312);
+            button8.Name = "button8";
+            button8.Size = new Size(70, 63);
+            button8.TabIndex = 1;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(80, 312);
+            button9.Name = "button9";
+            button9.Size = new Size(70, 63);
+            button9.TabIndex = 2;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(156, 312);
+            button10.Name = "button10";
+            button10.Size = new Size(70, 63);
+            button10.TabIndex = 3;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(232, 312);
+            button11.Name = "button11";
+            button11.Size = new Size(70, 63);
+            button11.TabIndex = 4;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(308, 312);
+            button12.Name = "button12";
+            button12.Size = new Size(70, 63);
+            button12.TabIndex = 5;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button3;
+        private Button button2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button1;
+        private ListView listView1;
+        private ColumnHeader user;
+        private ColumnHeader pos;
+        private ColumnHeader shift;
+        private ColumnHeader off_on;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button button8;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+    }
+}
